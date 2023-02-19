c18a2f87e1294ca5a2b43297ef0c2fefc7fdf7af
Bishmit Regmi <103090191+Bishmit@users.noreply.github.com>
2023-02-19 20:03:14 +0545

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -5,7 +5,7 @@ const int Size_of_board = 3;
 
 char board[Size_of_board][Size_of_board];
 
-void Set_board() {
+void set_board() {
     for (int row = 0; row < Size_of_board; row++) {
         for (int col = 0; col < Size_of_board; col++) {
             board[row][col] = ' ';
@@ -46,7 +46,7 @@ void update_board(int row, int col, char mark) {
     board[row][col] = mark;
 }
 
-	int check(char player_mark){
+	int win_condition(char player_mark){
 	// checking rows
 	if( board[0][0]==player_mark && board[0][1]==player_mark && board[0][2]==player_mark )
 		return 1;
@@ -73,30 +73,46 @@ void update_board(int row, int col, char mark) {
 	return 0;
 }
 
-int main() {
-    Set_board();
-    int winner = 0;
-    char player_mark = 'X';
-    while (1) {
-        display_board();
-        int row , col;
-        get_move(row, col);
-        update_board(row, col, player_mark);
-        winner = check(player_mark);
-        if (winner == 1) {
-            display_board();
-            cout << "Player " << (player_mark == 'X' ? 1 : 2) << " won the game!" << endl;
-            break;
-        } else if (winner == -1) {
-            display_board();
-            cout << "The game ended in a tie." << endl;
-            break;
-        }
-        if (player_mark == 'X') {
-            player_mark = 'O';
-        } else {
-            player_mark = 'X';
-        }       
-    }
-    return 0;
+int main(){
+	char key = 'Y';
+	while (key == 'Y' || key == 'y') {
+	int row, col; 
+	int winner = 0; 
+	char player_mark = 'X'; 
+	set_board();
+		while (true) {
+			system("cls");
+			display_board();
+			get_move(row, col);
+			update_board(row, col, player_mark);
+			
+			int winner = win_condition(player_mark);
+			if (winner == 1) {
+				display_board();
+				if (player_mark == 'X')
+					cout << "player 1 won the game " << endl;
+				else
+					cout << "player 2 won the game" << endl;
+
+				cout << endl << endl;
+				
+				cout << "Do you want to play again Y/N: ";
+				cin >> key;
+				system("cls");
+				if (key != 'y' || 'Y')
+					break;
+			}
+			else if (winner == -1) {
+				display_board();
+				cout << "The game ends in draw" << endl;
+				break;
+			}
+			if (player_mark == 'X')
+				player_mark = 'O';
+			else
+				player_mark = 'X';
+		}
+
+	}
+	return 0; 
 }
